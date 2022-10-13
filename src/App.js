2f7a4c7815c9e69a511131041e8f1551043000ad
Ran Pol <rnd809@gmail.com>
2022-10-13 14:40:36 -0400

    Add styles to the welcoming page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React from 'react'
 import './App.css'
-
+import blueBack from './images/blobsBlue.png'
+import yellowBack from './images/blobYellow.png'
 function App() {
-  return <div className="App">KLK</div>
+  const [start, setStart] = React.useState(true)
+
+  const startQuiz = () => setStart((prev) => !prev)
+
+  return (
+    <>
+      {start ? (
+        <div className="container--start">
+          <h1 className="start--title">Quizzical</h1>
+          <p className="start--summary">Are you ready to start?</p>
+          <button className="start--button" onClick={startQuiz}>
+            Start quiz
+          </button>
+          <img
+            className="start--img img-blue"
+            src={blueBack}
+            alt="blobBLue"
+          ></img>
+          <img
+            className="start--img img-yellow"
+            src={yellowBack}
+            alt="blobYellow"
+          ></img>
+        </div>
+      ) : (
+        <p>KLK</p>
+      )}
+    </>
+  )
 }
 
 export default App
